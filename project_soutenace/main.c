ead5a8d91e7d6b9dc26edcac6fd2cf79ea6d375b
ABDELMOUNAIM <abdelmounaimoub12@gmail.com>
2024-10-02 18:02:55 +0200

    V1

---
+++ project_soutenace/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("1. Ajouter une réservation : \n:");
+     printf("2. Modifier ou supprimer une réservation : \n:");
+      printf("3. Afficher les détails d'une réservation : \n:");
+       printf("4. Tri des réservations : \n:");
+         printf("5. Recherche des réservations :: \n:");
+           printf("6. Statistiques :: \n:");
+
+    return 0;
+}
